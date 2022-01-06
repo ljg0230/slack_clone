56c15f23c0286ab092fbdafc7a4e9030ad1ded92
ljg <ljg0230@gmail.com>
2022-01-06 21:38:53 +0900

    websocket 수정 중

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -16,7 +16,7 @@ import {
   WorkspaceModal,
 } from '@layouts/Workspace/styles';
 import axios from 'axios';
-import React, { useCallback, useState, VFC } from 'react';
+import React, { useCallback, useEffect, useState, VFC } from 'react';
 import { Redirect, Route, Switch, useParams } from 'react-router';
 import useSWR from 'swr';
 import gravatart from 'gravatar';
@@ -33,6 +33,7 @@ import InviteWorkspaceModal from '@components/InviteWorkspaceModal';
 import InviteChannelModal from '@components/InviteChannelModal';
 import ChannelList from '@components/ChannelList';
 import DMList from '@components/DMList';
+import useSocket from '@hooks/useSocket';
 
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
@@ -56,6 +57,18 @@ const Workspace: VFC = () => {
   } = useSWR<IUser | false>('/api/users', fetcher, { dedupingInterval: 2000 });
   const { data: channelData } = useSWR<IChannel[]>(userData ? `/api/workspaces/${workspace}/channels` : null, fetcher);
   const { data: memberData } = useSWR<IUser | false>(userData ? `/api/workspaces/${workspace}/members` : null, fetcher);
+  // const [socket, disconnect] = useSocket(workspace);
+
+  // useEffect(() => {
+  //   if (channelData && userData && socket) {
+  //     socket.emit('login', { id: userData.id, channels: channelData.map((v) => v.id) });
+  //   }
+  // }, [socket, channelData, userData]);
+  // useEffect(() => {
+  //   return () => {
+  //     disconnect();
+  //   };
+  // }, [workspace, disconnect]);
 
   const onLogout = useCallback(() => {
     axios
